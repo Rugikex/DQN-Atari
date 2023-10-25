cbaf86e14d595c6fde8c9a275e60666cb34f85b5
Rugikex <tanguy.malandain@epita.fr>
2023-10-25 14:41:40 +0200

    StackedFrames: fix reset fonction

--- stacked_frames.py
+++ stacked_frames.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 
 class StackedFrames():
-    def __init__(self, stack_size):
+    def __init__(self, stack_size: int):
         self.frames: deque = deque(maxlen=stack_size)
         self.previous_frame: np.ndarray = np.zeros((210, 160, 3))
 
@@ -29,6 +29,7 @@ class StackedFrames():
         return np.stack(self.frames, axis=-1)
     
     def reset(self, frame: np.ndarray) -> None:
+        self.previous_frame = np.zeros((210, 160, 3))
         initial_frame = self._preprocess_frame(frame)
         for _ in range(self.frames.maxlen):
             self.frames.append(initial_frame)
